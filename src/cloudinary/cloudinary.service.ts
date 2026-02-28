0228e99e20c69b69d8cce06b279698c990cfef3c
Usmon <abdullaevusmon2006@gmail.com>
2026-02-28 22:33:03 +0500

    fix type

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { v2 as cloudinary, UploadApiResponse } from 'cloudinary';
 import * as streamifier from 'streamifier';
+import { Express } from 'express';
 
 @Injectable()
 export class CloudinaryService {
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -14,6 +14,7 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import { Express } from 'express';
 
 import { GetUser } from 'src/common/decorators/get-user.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
