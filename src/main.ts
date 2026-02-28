4ae50c915cca8ab68a84e284d8dc98737308fc91
Usmon <abdullaevusmon2006@gmail.com>
2026-02-28 21:40:42 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -53,6 +53,6 @@ async function bootstrap() {
     SwaggerModule.createDocument(app, config, { ignoreGlobalPrefix: true }),
   );
 
-  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
